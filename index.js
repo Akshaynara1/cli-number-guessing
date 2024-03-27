6803127c72549d23b19a6abef45312f4fb2757a8
Akshaynara1 <160248610+Akshaynara1@users.noreply.github.com>
2024-03-26 20:22:46 -0700

    cli based number guessing game

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        message: "please guess a number betwwen 1-6",
+        type: "number",
+        name: "userGuessedNumber",
+    },
+]);
+console.log(answer);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guessed right number");
+}
+else {
+    console.log("alas! you guessed wrong number ");
+}
+;
